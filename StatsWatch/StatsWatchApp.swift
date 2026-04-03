3905cf71aa9d57ec9c16c842ae8df9183491a21f
WilliamGuo2002 <yg17@ualberta.ca>
2026-04-03 13:25:59 -0600

    Initial Commit

---
+++ StatsWatch/StatsWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsWatchApp.swift
+//  StatsWatch
+//
+//  Created by William Kuo on 2026-04-03.
+//
+
+import SwiftUI
+
+@main
+struct StatsWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatsWatchTests/StatsWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  StatsWatchTests.swift
+//  StatsWatchTests
+//
+//  Created by William Kuo on 2026-04-03.
+//
+
+import Testing
+
+struct StatsWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
